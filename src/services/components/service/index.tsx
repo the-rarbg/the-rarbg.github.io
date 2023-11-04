ef55e1ee4b62282cc3f47b3048e77592f0cec26b
the-rarbg <136214747+the-rarbg@users.noreply.github.com>
2023-11-04 10:06:11 +0530

    itemname

--- src/services/components/service/index.tsx
+++ src/services/components/service/index.tsx
@@ -43,7 +43,7 @@ const ServiceItem: FunctionComponent<ServiceItemProps> = ({ item }) => {
                 <Icon />
                 var domain = `https://${item.name}`
                 <div className="w-full flex justify-between items-baseline">
-                    <p className="ml-4 text-base font-semibold leading-6 text-gray-900"><a href={domain}>{item.name}</a></p>
+                    <p className="ml-4 text-base font-semibold leading-6 text-gray-900"><a href={item.name}>{item.name}</a></p>
                     <p className='text-xs text-gray-400 items-baseline	self-baseline'> {calculateUpTime()}% operational in last 90 days</p>
                 </div>
 
