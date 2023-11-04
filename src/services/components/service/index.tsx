192a993560fbcdf37ca3d2be14864573a93b1b9f
the-rarbg <136214747+the-rarbg@users.noreply.github.com>
2023-11-04 09:43:22 +0530

    link

--- src/services/components/service/index.tsx
+++ src/services/components/service/index.tsx
@@ -42,7 +42,7 @@ const ServiceItem: FunctionComponent<ServiceItemProps> = ({ item }) => {
             <div className='flex'>
                 <Icon />
                 <div className="w-full flex justify-between items-baseline">
-                    <p className="ml-4 text-base font-semibold leading-6 text-gray-900">{item.name}</p>
+                    <p className="ml-4 text-base font-semibold leading-6 text-gray-900"><a href="https://{item.name}">{item.name}</a></p>
                     <p className='text-xs text-gray-400 items-baseline	self-baseline'> {calculateUpTime()}% operational in last 90 days</p>
                 </div>
 
